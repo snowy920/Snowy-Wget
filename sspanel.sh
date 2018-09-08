51893caf3f5b2943679a67a211c6d6c64fa3c687
snowy920 <36103007+snowy920@users.noreply.github.com>
2018-09-08 19:29:59 +0800

    Add files via upload

---
+++ sspanel.sh
@@ -0,0 +1,14 @@
+echo 欢迎使用ss-panel-v3-mod一键安装脚本
+echo 使用前请确保已安装好生产环境
+stty erase '^H' && read -p "(请输入网站目录最后一项):" mulu
+cd /www/wwwroot/${mulu}
+yum -y install git
+git clone -b master https://github.com/NimaQu/ss-panel-v3-mod_Uim.git tmp && mv tmp/.git . && rm -rf tmp && git reset --hard
+chown -R root:root *
+chmod -R 755 *
+chown -R www:www storage
+php composer.phar install
+mv tool/alipay-f2fpay vendor/
+mv -f tool/cacert.pem vendor/guzzle/guzzle/src/Guzzle/Http/Resources/
+mv -f tool/autoload_classmap.php vendor/composer/
+cp config/.config.php.example config/.config.php
